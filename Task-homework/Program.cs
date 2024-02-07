25fcb743a68af7f4758133c6f1e7d1e5b81b4a0d
unknown <asgarkhanmb@code.edu.az>
2024-02-07 22:10:47 +0400

    task homework

---
+++ Task-homework/Program.cs
@@ -0,0 +1,190 @@
+﻿
+//1) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın.
+
+
+
+//int n = 21;
+
+//if (n % 3 == 0 && n % 7 == 0)
+
+//{
+//    Console.WriteLine(true);
+//}
+
+//else if (n % 3 == 1 && n % 7 == 1)
+
+//    Console.WriteLine(false);
+
+
+
+//2) n və m ədədləri verilir. Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+
+
+
+//int n=2;
+//int m=8;
+//int sum = 0;
+
+//if (n % 2 == 0 && m % 2 == 0)
+
+
+//   sum = n + m;
+
+//bool cut = true;
+
+//{
+
+// Console.WriteLine(sum);
+
+//}
+
+
+
+
+//3) Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+
+
+
+//int n = 5;
+//int m = 20;
+//int sum = 0;
+
+//for (int i = n; i <= m; i++)
+
+//    if (i % 2 == 1)
+//        sum += i;
+//{
+//    Console.WriteLine(sum);
+
+
+
+
+//4) Verilmish arrayin icindeki tek ededlerin cemini tapin.
+
+
+//}
+//int [] nums={ 3, 4, 5, 6, 7, 8, 9, 10 };
+//int sum = 0;
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i]%2==1)
+//    {
+//        sum+=nums[i];
+//    }
+//}
+//    Console.WriteLine(sum);
+
+
+
+
+//5) Verilmish arrayin icindeki cut ededlerin sayini tapin.
+
+
+
+//int[] nums = { 3, 4, 5, 6, 7, 8, 9, 10 };
+//int sum = 0;
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] % 2 == 0)
+//    {
+//        sum += nums[i];
+//    }
+//}
+//Console.WriteLine(sum);
+
+
+
+
+//6) Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin sayini tapin.
+
+
+//int n = 5;
+//int m = 20;
+//int count = 0;
+
+//for (int i = n; i < m; i++)
+
+//    if (i % 2 == 1)
+
+//        count ++;
+
+//    Console.WriteLine(count);
+
+
+
+
+
+//7) Verilmis n ve m (n<m) ededleri arasindaki cut ededlerin cemini tapin.
+
+//int n = 10;
+//int m = 20;
+//int sum = 0;
+
+//for (int i = n; i <= m; i++)
+
+//    if (i % 2 == 0)
+//        sum += i;
+//{
+//    Console.WriteLine(sum);
+//}
+
+
+
+
+//8) Verilmis n ve m (n<m) ededleri arasindaki cut ededlerin sayini tapin.
+
+
+//int n = 2;
+//int m = 20;
+//int count = 0;
+
+//for (int i = n; i < m; i++)
+
+//    if (i % 2 == 0)
+
+//        count++;
+
+//{
+//    Console.WriteLine(count);
+//}
+
+
+
+//9) Verilmis n tam ededinin sade ve ya murekkeb oldugunu tapin.
+
+//int n = 10;
+//int count = 0;
+
+//if (n < 2)
+//{
+//    bool simple=false;
+//    bool complex = false;
+//}
+//{
+//    Console.WriteLine(false);
+//}
+
+//for (int i =0; i <= n; i++)
+
+//    if(n % i== 0)
+
+//      count++;
+
+//Console.WriteLine(count);
+//    if (count == 2)
+//{ 
+//bool simple=true;
+
+//bool complex=false;
+
+
+//    Console.WriteLine(true); 
+//}
+
+//  else
+//{
+//    Console.WriteLine(false);
+
+//        }

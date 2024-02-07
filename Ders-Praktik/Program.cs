2125543e8ac73b29827d8e5b05311b8aba8ec10f
unknown <asgarkhanmb@code.edu.az>
2024-02-07 23:21:33 +0400

    ders praktik

---
+++ Ders-Praktik/Program.cs
@@ -0,0 +1,94 @@
+﻿//using System.Runtime.InteropServices;
+
+//int age = 30;
+
+//Console.WriteLine(age);
+
+//int n = 10;
+//int m = 100;
+
+//int sum = n + m;
+
+//Console.WriteLine(sum);
+
+//bool isMarried = true;
+
+//if(isMarried == true)
+//{
+//    Console.WriteLine("yes");
+
+//}
+//else
+//{
+//    Console.WriteLine("no");
+//}
+
+
+
+
+//int[] arr = { 1, 2, 3, 4, 5 };
+
+//for (int i = 0; i < arr.Length; i++)
+
+//{
+//    Console.WriteLine(i);
+//}
+
+
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    Console.WriteLine(arr[i]);
+//}
+
+
+
+
+
+//int a = 1;
+//int b = 100;
+
+//for (int i = a;i <=b; i++)
+//{
+//    Console.WriteLine(i);   
+//}
+
+
+
+//int a = 1;
+//int b = 100;
+
+//for (int i = a; i <= b; i++)
+
+//    if (i % 2 == 0)
+//   {
+//        Console.WriteLine(i);
+//    }
+
+//int a = 1;
+//int b = 10;
+//int result = 0;
+
+//for (int i = 0; i <= b; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        result += i;
+//    }
+
+//}
+//Console.WriteLine(result);
+
+
+//string[] words = { "1", "1", "5", "1","1","1","2","1"};
+//int count = 0;
+//for (int i = 0; i < words.Length; i++)
+//{
+//    if (words[i] == "1")
+
+//    {
+
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
